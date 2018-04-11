1c99d9ab8bbdead7e9fe92534fde20fec4d809f6
ZjpNode <461584040@qq.com>
2018-04-11 11:55:31 +0800

    1、重新定义hapi route参数验证中的错误回调failAction； 2、新增获取配置信息的方法； 3、新增返回参数格式化功能；

--- common/util.js
+++ common/util.js
@@ -1,17 +1,13 @@
-var fs = require("fs");
-var path = require("path");
-
-//解析需要遍历的文件夹，我这以E盘根目录为例
-// var filePath = path.resolve("E:");
-
-//调用文件遍历方法
-// fileDisplay(filePath);
-
+const fs = require("fs");
+const path = require("path");
+const Boom = require("boom");
+const CONSTANT = require(path.resolve("./common/constant"));
 /**
  * 文件遍历方法
- * @param filePath 需要遍历的文件路径
+ * @param {*} filePath 需要遍历的文件夹
+ * @param {*} fileObj  返回的文件对象，默认为空
+ * @returns { Array } filePath目录下的所有文件
  */
-
 function fileDisplay(filePath, fileObj) {
   //根据文件路径读取文件，返回文件列表
   let fileList = fileObj || [];
@@ -35,6 +31,60 @@ function fileDisplay(filePath, fileObj) {
   return fileList;
 }
 
+/**
+ * 请求返回值格式化
+ * @param {Boolean} flag 成功与否的标识
+ * @param {Object} msg 返回的内容
+ * @returns {Object} 格式{ success: flag, data: msg, err: msg }
+ */
+function responseFormat(flag, msg) {
+  let result = { success: flag, data: "", err: "" };
+  if (flag) {
+    result.data = msg;
+  } else {
+    result.err = msg;
+  }
+  return result;
+}
+
+/**
+ * hapi的route.config.validate.failAction函数返回值格式化
+ * 由于使用了Boom模块，故只适用于自带Boom模块的hapi
+ * @param {string} errMsg 错误信息
+ * @returns Boom<Data> Boom错误对象
+ */
+function failActionReturnFormat(errMsg) {
+  let error = Boom.badData("your data is bad and you should feel bad"); // 422
+  // 修改http头的Status Code，定义为成功回调
+  error.output.statusCode = 200;
+  // 使error.output.statusCode = 200;生效
+  error.reformat();
+  // 用自定义的返回参数覆盖hapi自带的error、statusCode、message字段
+  error.output.payload = responseFormat(false, errMsg);
+  return error;
+}
+
+/**
+ * 从全局global配置信息global[CONSTANT.CONFIG]，
+ * 若global不存在配置信息，则require config下的配置文件,并放入缓存中
+ * @param {string} key common下文件的名称，不带后缀
+ * @returns {object} key对应的配置信息
+ */
+function getConfig(key) {
+  global[CONSTANT.CONFIG] = global[CONSTANT.CONFIG]
+    ? global[CONSTANT.CONFIG]
+    : {};
+  if (!global[CONSTANT.CONFIG][key]) {
+    global[CONSTANT.CONFIG][key] = require(path.resolve(
+      `${CONSTANT.CONFIG_DIR}/${key}.js`
+    ));
+  }
+  return global[CONSTANT.CONFIG][key];
+}
+
 module.exports = {
-  fileDisplay: fileDisplay
+  fileDisplay: fileDisplay,
+  responseFormat: responseFormat,
+  getConfig: getConfig,
+  failActionReturnFormat: failActionReturnFormat
 };
--- route/index.js
+++ route/index.js
@@ -1,5 +1,20 @@
+/*
+ * @Author: jiapeng.Zheng 
+ * @Date: 2018-04-11 09:13:19 
+ * @Last Modified by: jiapeng.Zheng
+ * @Last Modified time: 2018-04-11 11:50:50
+ * @description: 自动路由
+ *  1、遍历当前目录下除去本身(route\index.js)外的所有路由文件；
+ *  2、修正路由文件的path，以route目录层级为path的路径，
+ *     即route\api\userinfo.js的path为 /api/userinfo + 该文件的path；
+ *  3、重新定义各个路由文件中参数验证（config.validate）中的错误回调failAction，
+ *     若本身存在config.validate.failAction,则忽略以下规则，否则，
+ *     a、修改http头的Status Code，
+ *     b、用自定义的返回参数覆盖hapi自带的error、statusCode、message字段
+ */
 const path = require("path");
-const util = require("../common/util");
+const _ = require("lodash");
+const util = require(path.resolve("./common/util"));
 
 const routeFiles = util.fileDisplay(__dirname);
 let routeList = [];
@@ -9,14 +24,25 @@ routeFiles.forEach(routeFilePath => {
     return;
   } else {
     let route = require(routeFilePath);
+    // 修正hapi route中的path，规定以route目录为path的路径
     route.path =
       routeFilePath
         .replace(__dirname, "")
         .split(path.sep)
         .join("/")
         .replace(/.js$/g, "") + route.path;
+    // 重新定义hapi route参数验证中的错误回调failAction
+    let route_config_validate = {
+      failAction: function(request, h, err) {
+        return util.failActionReturnFormat(ERR.message);
+      }
+    };
+    if (route.config) {
+      route.config.validate = _.extend(route_config_validate, route.config.validate);
+    } else {
+      route.config = {validate:route_config_validate};
+    }
     routeList.push(route);
   }
 });
-console.log(routeList);
 module.exports = routeList;
