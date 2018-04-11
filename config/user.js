fcb7fc630659da47906e22dcd7bd964b5d631ac5
ZjpNode <461584040@qq.com>
2018-04-11 17:22:36 +0800

    新增use配置

---
+++ config/user.js
@@ -0,0 +1,19 @@
+let user = {
+  admin: {
+    password: "password",
+    userInfo: {
+      id: "asfg48a7f3a4f3iug",
+      loginName: "admin",
+      nickName: "郑先生",
+      age: 18,
+      sex: "男"
+    }
+  }
+};
+let useList = {
+  asfg48a7f3a4f3iug: "admin"
+};
+module.exports = {
+  user: user,
+  useList: useList
+};
